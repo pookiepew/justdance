e906bced4edb8df3528d823eba5783dc3357b228
pookiepew <glensorbo@gmail.com>
2021-02-10 20:18:11 +0100

    ✔ MongoDB - Deleted the isValid check as mongoose does that as well

--- MongoDB/functions/index.js
+++ MongoDB/functions/index.js
@@ -1,11 +1,9 @@
 const findUserByTwitchID = require('./findUserByTwitchID');
 const saveUser = require('./saveUser');
-const userIsValid = require('./userIsValid');
 const updateConnection = require('./updateConnection');
 
 module.exports = {
   findUserByTwitchID,
   saveUser,
-  userIsValid,
   updateConnection
 };
